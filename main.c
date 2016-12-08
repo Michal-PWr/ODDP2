8d378a1bb8e53ed56166859c85964fdfe8854f56
Michał Stroka <Micha Stroka>
2016-12-08 13:06:36 +0100

    Stan prac po dzisiejszych (12.08.2016) zajeciach

--- main.c
+++ main.c
@@ -4,42 +4,70 @@
  *  Created on: 20 paŸ 2016
  *      Author: Micha³
  */
-#include <stdio.h>			
+#include <stdio.h>			// T - nastêpnicy technologiczni
 
-#define MAX_ZADAN 100			
+#define MAX_ZADAN 100			// P - czasy wykonania operacji
 #define MAX_STANOWISK 20
 
-int WczytajZPliku(FILE *fp, int *iloscZadan, int *iloscStanowisk, int czasyZadan[MAX_ZADAN][MAX_STANOWISK])
+int WczytajZPliku(FILE *fp, int *iloscZadan, int *iloscStanowisk, int *czasyZadan)
 {
 	int i;
 	fscanf(fp, "%d", iloscZadan);
 	fscanf(fp, "%d", iloscStanowisk);
-	for (i = 0; i < *iloscZadan; i++)
+	for (i = 1; i <= (*iloscZadan) * (*iloscStanowisk); i++)
 	{
-		for (int j = 0; j < *iloscStanowisk; j++)
-		{
-			fscanf(fp, "%d", &czasyZadan[i][j]);
-		}
+		fscanf(fp, "%d", &czasyZadan[i]);
 	}
 	return 0;
 }
+void UstawPoczatkowaPermutacje(int *kolejnoscZadan, int iloscZadan, int iloscStanowisk)
+{
+	//Wszystkie zadania po kolei tylko na jednej maszynie ze stanowiska
+	int przesuniecie = 1;
+	for (int i =0, j = 0; j<iloscStanowisk; ++j)
+	{
+		for (i = 0; i< iloscZadan; ++i)
+		{
+			kolejnoscZadan[j*iloscZadan+i+przesuniecie] = (i*iloscStanowisk + j + 1);
+		}
+		kolejnoscZadan[j*i+i+przesuniecie] = 0;
+		przesuniecie++;
+		kolejnoscZadan[j*i+i+przesuniecie] = 0;
+		przesuniecie++;
+	}
+}
 
+int ObliczNastepnikowMaszynowych(int *A, int iloscStanowisk, int iloscZadan, int *kolejnoscZadan)
+{
+	for (int i = 1; i <= iloscZadan * iloscStanowisk + 2 * iloscStanowisk; i++)
+	{
+		A[kolejnoscZadan[i]] = kolejnoscZadan[i+1];
+	}
+	return 0;
+}
 int main()
 {
 	FILE *fp;
-	int czasyZadan[MAX_ZADAN][MAX_STANOWISK] = {{0}};
+	int A[MAX_ZADAN*MAX_STANOWISK+1] = {0}; // nastepnicy maszynowi
+	int czasyZadan[MAX_ZADAN*MAX_STANOWISK+1] = {0};
+	int kolejnoscZadan[MAX_ZADAN*MAX_STANOWISK+2*MAX_STANOWISK] = {0}; // jest to tablica zawieraj¹ca kolejnosc wykonywania zadan na maszynach. '-1' oddziela maszyny
 	int iloscZadan, iloscStanowisk;
 
-	fp = fopen("NEH9.DAT", "r");
+	fp = fopen("NEH1.DAT", "r");
 	WczytajZPliku(fp, &iloscZadan, &iloscStanowisk, czasyZadan);
-	for (int i=0; i<iloscZadan; i++)
+	for (int i=0; i<=(iloscZadan*iloscStanowisk); i++)
 	{
-		for (int j = 0; j<iloscStanowisk; j++)
-		{
-			printf("%d ", czasyZadan[i][j]);
-		}
-		printf("\n");
+		printf("%d ", czasyZadan[i]);
 	}
-
+	printf("\n");
+	UstawPoczatkowaPermutacje(kolejnoscZadan, iloscZadan, iloscStanowisk);
+	for (int i=0; i<iloscStanowisk*iloscZadan+2*iloscStanowisk; i++)
+		printf("%d ",kolejnoscZadan[i]);
+	ObliczNastepnikowMaszynowych(A, iloscStanowisk, iloscZadan, kolejnoscZadan);
+	printf("\n");
+	for (int i = 1; i<= iloscStanowisk*iloscZadan; i++ )
+	{
+		printf("%d ", A[i]);
+	}
 	return 0;
 }
